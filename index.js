470f3a567b9d5934adb2c206afb0a19b455a7c14
Di <dias.ospanov@gmail.com>
2023-01-11 15:01:09 +0200

    Check if folder exists

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello');
+//do {
+//  const memes = mkdir [c:]\Users\Alina\projects\node-meme-scraper\memes;
+//}
+//while
+import * as fs from 'fs';
+
+const checkFolder = (folderPath) => {
+  const isFolderExist = fs.existsSync(folderPath);
+  return isFolderExist;
+};
+console.log(checkFolder('/c/Users/Alina/projects/node-meme-scraper/memes'));
