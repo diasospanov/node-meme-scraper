d4bc659316e938aae5ebc5ce2eac4ecf8e1760b2
Di <dias.ospanov@gmail.com>
2023-01-13 15:15:29 +0200

    Create an array of unknown values

--- index.js
+++ index.js
@@ -9,15 +9,23 @@
 // Fetch an html from a website
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import $ from 'jquery';
 
 const websiteLink = 'https://memegen-link-examples-upleveled.netlify.app/';
-// const links = [];
+const links = [];
 async function getImgSrcs() {
   try {
     const response = await axios.get(websiteLink);
     const htmlContent = cheerio.load(response.data);
     const imageSrcs = htmlContent('section').html();
-    console.log(imageSrcs);
+    // src = imageSrcs(this).getElementById(img).text();
+    // links.push({ src });
+    htmlContent(imageSrcs).each(function () {
+      const href = (this.href = htmlContent(this).find('div a').text());
+      const src = (this.src = htmlContent(this).find('div img').text());
+      links.push({ href, src });
+    });
+    console.log(links);
   } catch (err) {
     console.error(err);
   }
