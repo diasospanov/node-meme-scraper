0b6e6e1f39a6f205fed57968768edaafa445f948
Di <dias.ospanov@gmail.com>
2023-01-14 10:39:01 +0200

    Download first image

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// Create a folder
-// import * as fs from 'fs';
-
-// const folderName = '/Users/Alina/projects/node-meme-scraper/memes';
-// if (!fs.existsSync(folderName)) {
-//   fs.mkdirSync(folderName);
-// }
-
-// Fetch an html from a website
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import * as fs from 'fs';
 import $ from 'jquery';
 
+// Create a folder
+const folderName = '/Users/Alina/projects/node-meme-scraper/memes';
+if (!fs.existsSync(folderName)) {
+  fs.mkdirSync(folderName);
+}
+
+// Fetch an html from a website and create an array
 const websiteLink = 'https://memegen-link-examples-upleveled.netlify.app/';
 const links = [];
 async function getImgSrcs() {
@@ -21,9 +20,35 @@ async function getImgSrcs() {
       const src = htmlContent(this).attr('src');
       links.push({ src });
     });
-    console.log(links);
+    const firstTenLinks = links.slice(0, 10);
+    async function downloadImgs() {
+      await axios({
+        method: 'get',
+        url: firstTenLinks[0].src,
+        responseType: 'stream',
+      }).then(function (file) {
+        file.data.pipe(
+          fs.createWriteStream(
+            '/Users/Alina/projects/node-meme-scraper/memes/01.jpg',
+          ),
+        );
+      });
+    }
+    await downloadImgs();
+    console.log('Done!');
+    // console.log(firstTenLinks);
   } catch (err) {
     console.error(err);
   }
 }
 await getImgSrcs();
+// async function downloadImgs() {
+//   await axios({
+//     method: 'get',
+//     url: firstTenLinks[0].src,
+//     responseType: 'stream',
+//   }).then(function (response) {
+//     response.data.pipe(fs.createWriteStream('01.jpg', '.memes'));
+//   });
+// }
+// await downloadImgs();
